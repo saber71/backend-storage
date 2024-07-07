49988050e85c1821cb35c39532165a9990e73ba8
saber71 <1533357473@qq.com>
2024-07-07 22:03:50 +0800

    feat(backend-storage): 初始化backend-storage模块并添加基本架构
    初始化了backend-storage模块的源码目录和测试目录，并在.code-hub.iml和.vcs.xml文件中进行了配置。同时，添加了基本的路由、指数和保存功能的实现，以及相关依赖的package.json和tsconfig.json文件。这为backend-storage模块的后续开发和扩展提供了基础架构支持。

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import Koa from "koa"
+import { koaBody } from "koa-body"
+import { router } from "./routes"
+
+export const app = new Koa()
+
+app
+  .use(
+    koaBody({
+      multipart: true,
+      formidable: { keepExtensions: true, multiples: true }
+    })
+  )
+  .use(router.routes())
+  .use(router.allowedMethods())
---
+++ www.js
@@ -0,0 +1,5 @@
+import { app } from "./dist"
+
+const port = 10000
+console.log("backend-storage", "start at port:", port)
+app.listen(port)
